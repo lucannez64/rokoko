beb78dca0b8994fca1d490d87de7c8d224141bc3
Lorenzo Tucci <lorenzo@unitoo.it>
2025-11-18 18:10:27 +0200

    inline is ignored in bindings def

--- src/hexl/bindings.rs
+++ src/hexl/bindings.rs
@@ -1,45 +1,31 @@
 #[link(name = "hexl_wrapper")]
 extern "C" {
-    #[inline]
     pub fn multiply_mod(a: u64, b: u64, modulus: u64) -> u64;
 
-    #[inline]
     pub fn power_mod(a: u64, b: u64, modulus: u64) -> u64;
 
-    #[inline]
     pub fn add_mod(a: u64, b: u64, modulus: u64) -> u64;
 
-    #[inline]
     pub fn sub_mod(a: u64, b: u64, modulus: u64) -> u64;
 
-    #[inline]
     pub fn eltwise_mult_mod(result: *mut u64, operand1: *const u64, operand2: *const u64, n: u64, modulus: u64);
 
-    #[inline]
     pub fn get_roots(n: u64, modulus: u64) -> *const u64;
 
-    #[inline]
     pub fn inv_mod(a: u64, modulus: u64) -> u64;
 
-    #[inline]
     pub fn get_inv_roots(n: u64, modulus: u64) -> *const u64;
 
-    #[inline]
     pub fn eltwise_add_mod(result: *mut u64, operand1: *const u64, operand2: *const u64, n: u64, modulus: u64);
 
-    #[inline]
     pub fn eltwise_sub_mod(result: *mut u64, operand1: *const u64, operand2: *const u64, n: u64, modulus: u64);
 
-    #[inline]
     pub fn eltwise_mod_inverse(result: *mut u64, operand1: *const u64, n: u64, modulus: u64);
 
-    #[inline]
     pub fn multiply_poly(result: *mut u64, operand1: *const u64, operand2: *const u64, n: u64, modulus: u64);
 
-    #[inline]
     pub fn eltwise_reduce_mod(result: *mut u64, operand: *const u64, n: u64, modulus: u64);
 
-    #[inline]
     pub fn polynomial_multiply_cyclotomic_mod(result: *mut u64, operand1: *const u64, operand2: *const u64, phi: u64, mod_q: u64);
     pub fn ntt_forward_in_place(
         operand: *mut u64,
@@ -47,13 +33,11 @@ extern "C" {
         modulus: u64,
     );
 
-    #[inline]
     pub fn ntt_inverse_in_place(
         operand: *mut u64,
         n: usize,
         modulus: u64,
     );
 
-    #[inline]
     pub fn eltwise_fma_mod(result: *mut u64, operand1: *const u64, operand2: u64, operand3: *const u64, n: u64, modulus: u64);
 }
