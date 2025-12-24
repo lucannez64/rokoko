74d1bb196c2dab5988e88c6ee7a1daac2e93db8d
osdnk <micosa97@gmail.com>
2025-12-24 17:07:33 +0100

    opening

--- src/protocol/execution.rs
+++ src/protocol/execution.rs
@@ -1,11 +1,15 @@
+use num::range;
+
 use crate::{
     common::{
-        matrix::VerticallyAlignedMatrix, ring_arithmetic::Representation,
+        matrix::VerticallyAlignedMatrix,
+        ring_arithmetic::{Representation, RingElement},
         sampling::sample_random_short_vector,
     },
     protocol::{
         commitment::{self, commit},
         crs::CRS,
+        open::open_at,
     },
 };
 
@@ -19,4 +23,14 @@ pub fn execute() {
     };
 
     let commitment = commit(&crs, &witness);
+
+    let evaluation_points_inner = vec![range(0, witness.height.ilog2() as usize)
+        .map(|_| RingElement::random_bounded(Representation::IncompleteNTT, 2))
+        .collect::<Vec<RingElement>>()];
+
+    let evaluation_points_outer = vec![range(0, witness.width.ilog2() as usize)
+        .map(|_| RingElement::random_bounded(Representation::IncompleteNTT, 2))
+        .collect::<Vec<RingElement>>()];
+
+    let opening = open_at(&witness, &evaluation_points_inner, &evaluation_points_outer);
 }
