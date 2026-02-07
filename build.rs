f101cff58a5b8f19a67fe3f7731614ae83b4dbee
Michał Osadnik <micosa97@gmail.com>
2026-02-07 10:48:29 +0200

    remove hexl wrapper in favour of conditional linking

--- build.rs
+++ build.rs
@@ -6,9 +6,23 @@ fn main() {
     // shared library built via `make hexl && make wrapper`.
     #[cfg(not(feature = "rust-hexl"))]
     {
+        let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+
         println!("cargo:rustc-link-lib=dylib=hexl_wrapper");
         println!("cargo:rustc-link-search=native=.");
         println!("cargo:rustc-link-search=native=./hexl-bindings/hexl/build/hexl/lib");
         println!("cargo:rustc-link-search=native=./hexl-bindings/hexl/build/hexl/lib64");
+
+        // Embed rpath so the binary can find shared libraries at runtime
+        // without needing to set LD_LIBRARY_PATH
+        println!("cargo:rustc-link-arg=-Wl,-rpath,{}", manifest_dir);
+        println!(
+            "cargo:rustc-link-arg=-Wl,-rpath,{}/hexl-bindings/hexl/build/hexl/lib",
+            manifest_dir
+        );
+        println!(
+            "cargo:rustc-link-arg=-Wl,-rpath,{}/hexl-bindings/hexl/build/hexl/lib64",
+            manifest_dir
+        );
     }
 }
--- cargo_wrapper.sh
+++
@@ -1,4 +0,0 @@
-# !/bin/bash
-export LD_LIBRARY_PATH="./hexl-bindings/hexl/build/hexl/lib:$(pwd):/usr/local/lib"
-
-RUSTFLAGS=-Awarnings cargo "$@"
