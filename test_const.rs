c0526029bbdc7d41b7dbd632ae5ecd2bd2c9cc25
Michał Osadnik <micosa97@gmail.com>
2026-02-09 14:32:57 +0200

    remove old file

--- test_const.rs
+++
@@ -1,9 +0,0 @@
-const PROJECTION_BASE_HEIGHT: usize = 8;
-const SIZE: usize = PROJECTION_BASE_HEIGHT * PROJECTION_BASE_HEIGHT / 8;
-
-fn main() {
-    println!("PROJECTION_BASE_HEIGHT = {}", PROJECTION_BASE_HEIGHT);
-    println!("SIZE = {}", SIZE);
-    let arr: [u8; SIZE] = [0; SIZE];
-    println!("Array length: {}", arr.len());
-}
